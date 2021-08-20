5faa65dc9e34dd65b1ffae4977905ee16671b883
javierpb275 <javierpb275@gmail.com>
2021-08-20 16:27:47 +0200

    used useState to create an be able to set a boolean and render the modal and the backdrop components conditionally.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import Backdrop from "./components/backdrop.component";
-import Modal from "./components/modal.component";
 import Todo from "./components/todo.component";
 
 function App() {
   return (
     <div className="App">
       <h1>My Todos</h1>
-     <Todo text="Learn React"/>
-     <Todo text="Master React"/>
-     <Todo text="Learn Next.js"/>
-     <Todo text="Master Next.js"/>
-     <Modal/>
-     <Backdrop/>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Learn Next.js" />
+      <Todo text="Master Next.js" />
     </div>
   );
 }
--- src/components/todo.component.jsx
+++ src/components/todo.component.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import Modal from "./modal.component";
+import Backdrop from "./backdrop.component";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const deleteHandler = () => {
-    console.log(props.text)
-  }
+    setModalIsOpen(true);
+  };
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {
+        // modalIsOpen? <Modal/> : null
+        modalIsOpen && <Modal />
+      }
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
