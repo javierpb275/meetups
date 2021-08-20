2edbf0dd7e7f4f02f96ef5e65b8119abf6b5d378
javierpb275 <javierpb275@gmail.com>
2021-08-20 15:47:12 +0200

    created todo component and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import Todo from "./components/todo.component";
+
 function App() {
   return (
     <div className="App">
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+     <Todo/>
     </div>
   );
 }
