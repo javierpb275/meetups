5ad389af39bf4c2d2aa1806ba3e19fea74c1373f
javierpb275 <javierpb275@gmail.com>
2021-08-22 16:11:52 +0200

    Creaated main navigation component. Added links to different pages. imported and displayed main navigation component in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Route, Switch} from 'react-router-dom';
 import AllMeetupsPage from './pages/all-meetups.page';
 import NewMeetupPage from './pages/new-meetup.page';
 import FavoritesPage from './pages/favorites.page';
+import MainNavigation from './components/layout/MainNavigation.component';
 
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Switch>
       <Route exact path='/'>
         <AllMeetupsPage/>
