39b2b80c2ef1ae73d94ded238bf8a8d33daf89da
javierpb275 <javierpb275@gmail.com>
2021-08-22 14:12:00 +0200

    removed code and files to start making the meetups app.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Todo from "./components/todo.component";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Learn Next.js" />
-      <Todo text="Master Next.js" />
+    <div>
     </div>
   );
 }
