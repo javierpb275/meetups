f97be65f5e5b79f9e01e1bbcee50177a04129ebd
javierpb275 <javierpb275@gmail.com>
2021-08-26 17:02:05 +0200

    navigating programmatically using useHistory hook from react-router-dom

--- src/pages/new-meetup.page.jsx
+++ src/pages/new-meetup.page.jsx
@@ -1,6 +1,9 @@
 import NewMeetupForm from "../components/meetups/new-meetup-form.component";
+import { useHistory } from "react-router-dom";
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
   const addMeetupHandler = (meetupData) => {
     fetch("https://meetups-45992-default-rtdb.firebaseio.com/meetups.json", {
       method: "POST",
@@ -8,6 +11,8 @@ const NewMeetupPage = () => {
       headers: {
         "Content-Type": "application/json",
       },
+    }).then(() => {
+      history.replace("/");
     });
   };
 
